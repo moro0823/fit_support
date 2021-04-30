4c2ba455bf8b81ff2e83ac16365b088c12976053
EC2 Default User <ec2-user@ip-172-31-17-104.ap-northeast-1.compute.internal>
2021-04-30 06:09:31 +0000

    [Finish] devise admin&customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource_or_scope)
+    # resource_or_scope.is_a? => deviseのモデルが複数ある場合に使用
+    if resource_or_scope.is_a?(AdminUser)
+        root_path
+    else
+        root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    return new_admin_user_session_path if resource_or_scope == :admin_user
+    root_path
+  end
+
 end
