16099660ccaa7cac789556aa453c723519b74004
moro0823 <basell.4rm.0823@outlook.com>
2021-05-23 05:40:59 +0000

    rubocop

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -10,16 +10,17 @@ class Public::SearchesController < ApplicationController
   end
 
   def search_post
-    @posts = Post.where('title LIKE ? OR body LIKE ?',"%#{params[:search]}%", "%#{params[:search]}%" ).page(params[:page]).reverse_order
+    @posts = Post.where('title LIKE ? OR body LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).reverse_order
   end
 
   def search_mypage_post
-    @posts = Post.where( "status LIKE ?", "%#{params[:search]}%" ).where(customer_id: current_customer).page(params[:page]).reverse_order
-    @status_title = Post.find_by( "status LIKE ?", "%#{params[:search]}%" )
+    @posts = Post.where("status LIKE ?", "%#{params[:search]}%").where(customer_id: current_customer).page(params[:page]).reverse_order
+    @status_title = Post.find_by("status LIKE ?", "%#{params[:search]}%")
     # @posts 投稿一覧表示用 @status_title タイトル表示用
   end
 
   private
+
   # fetchはparams[:search]が空の場合{}をparams[:search]が空でない場合、params[:search]を返す
   def customer_search_params
     params.fetch(:search, {}).permit(:sex, :age_from, :age_to, :height_from, :height_to,
@@ -30,5 +31,4 @@ class Public::SearchesController < ApplicationController
   def customer_search_friend_params
     params.fetch(:search_friend, {}).permit(:username)
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,4 @@ class Post < ApplicationRecord
   validates :status, inclusion: { in: ["トレーニングメニュー", "食事", "情報の共有"] }
   has_many :post_images, dependent: :destroy
   accepts_attachments_for :post_images, attachment: :image
-
 end
